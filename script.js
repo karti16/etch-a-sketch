5b18362b33c1d4fa26149671f2ad8cef23986bba
karti16 <kartim316@gmail.com>
2022-03-25 15:13:10 +0530

    added slide to input gride size instead for text box

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
 let mainContainer = document.querySelector(".main-container");
+let gridCount = 20;
+let slider = document.getElementById("myRange");
+let sliderValue = document.getElementById("gridvalue");
+sliderValue.innerHTML = slider.value;
+let clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearPaint);
 
-let gridCount = 16;
-// createRow();
-generateGrin(gridCount);
-setWidth();
-red();
+// Calling main function to start with default grid size
+main();
 
+// Main function. calling all other fuction
+function main() {
+  generateGrin(gridCount);
+  setWidth();
+  paint();
+}
+
+slider.oninput = function () {
+  gridCount = this.value;
+  sliderValue.innerHTML = gridCount;
+  console.log(mainContainer);
+  main();
+};
+
+// Generate Grid and attach to html
 function generateGrin(size) {
   const box = `<div class="box"></div>`.repeat(size);
   const row = `<div class="row-container">${box}</div>`.repeat(size);
   mainContainer.innerHTML = row;
 }
 
-// function createRow() {
-//   for (let i = 1; i <= gridCount; i++) {
-//     let d = document.createElement("div");
-//     d.classList = `row-container`;
-
-//     mainContainer.append(d);
-//   }
-//   rowCont = document.querySelectorAll(".row-container");
-//   createBox(gridCount, rowCont);
-// }
-
-// function createBox() {
-//   for (row of rowCont) {
-//     for (let i = 1; i <= gridCount; i++) {
-//       let d = document.createElement("div");
-//       d.classList = `box`;
-//       row.append(d);
-//     }
-//   }
-// }
-
+// Set width of the boxes after generating it
 function setWidth() {
   boxes = document.querySelectorAll(".box");
   for (let i = 0; i < boxes.length; i++) {
-    boxes[i].style.width = `${mainContainer.offsetWidth / gridCount}px`;
-    boxes[i].style.height = `${mainContainer.offsetWidth / gridCount}px`;
+    boxes[i].style.width = `${mainContainer.scrollWidth / gridCount}px`;
+    boxes[i].style.height = `${Math.floor(
+      mainContainer.scrollHeight / gridCount
+    )}px`;
+    console.log(mainContainer.scrollHeight);
   }
 }
 
-function red() {
+// Starts painting with mouse click and mouseclick and mouse move
+function paint() {
   for (let i = 0; i < boxes.length; i++) {
     console.log();
     boxes[i].addEventListener("mousedown", function () {
@@ -55,15 +57,14 @@ function red() {
   }
 }
 
-function noRed() {
+// Clears the paint already painted
+function clearPaint() {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].style.backgroundColor = "";
   }
 }
 
-let clearBtn = document.querySelector(".clear");
-clearBtn.addEventListener("click", noRed);
-
+// Helps to check if mouse is draging or not
 let mouseDown = false;
 let mouseMove = false;
 
@@ -74,23 +75,26 @@ mainContainer.addEventListener("mouseup", () => {
   mouseMove = false;
 });
 
-function getSize() {
-  gsize = +document.querySelector("#grid-size").value;
-  clearGrid();
+// No longer needed as the slider replace this function
+// function getSize() {
+//   gsize = +document.querySelector("#grid-size").value;
+//   clearGrid();
 
-  gridCount = gsize;
-  console.log(gridCount);
-  mainContainer = document.querySelector(".main-container");
-  // createRow();
-  generateGrin(gridCount);
-  setWidth();
-  red();
-}
+//   gridCount = gsize;
+//   console.log(gridCount);
+//   mainContainer = document.querySelector(".main-container");
+//   // createRow();
+//   generateGrin(gridCount);
+//   setWidth();
+//   red();
+// }
 
+//Clear the grid before generating other size grids
 function clearGrid() {
   mainContainer.innerHTML = "";
 }
 
+// Generated random rgb color to paint in rainbow color
 function random_rgba() {
   var o = Math.round,
     r = Math.random,
